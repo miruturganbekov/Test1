7fd8ffa9a7968631ff8a405e63ea289796a56bb8
miruturganbekov <167768910+miruturganbekov@users.noreply.github.com>
2024-05-06 16:41:05 +0000

    First Comit

---
+++ sss/my-app/src/App.js
@@ -0,0 +1,17 @@
+// import './App.css';
+
+import {Card} from "./componets/card";
+
+
+function App() {
+  return (
+    <div className='App'>
+    <Card
+    full_name="Mirulan"
+    postion="Js ins"/>
+    
+    </div>
+  );
+}
+
+export default App;
